2dcd66f2e68f2c7580097b1bf0e8e539ed9e9209
erikpetrosyan7 <erikpetrosyann7@gmail.com>
2024-09-29 13:44:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 // 1
-// let n = 2345;
-// let sum = 0;
+let n = 2345;
+let sum = 0;
 
-// while (n > 0) {
-// 	console.log(n % 10);
+while (n > 0) {
+	console.log(n % 10);
 
-// 	n = (n - (n % 10)) / 10;
-// 	sum += n % 10;
-// }
-// console.log(sum);
+	n = (n - (n % 10)) / 10;
+	sum += n % 10;
+}
+console.log(sum);
 
-// let a = 1099;
-// let sum2 = 0;
+let a = 1099;
+let sum2 = 0;
 
-// for (; a > 0; a = Math.floor(a / 10)) {
-// 	console.log(a % 10);
-// 	sum2 += a % 10;
-// }
-// console.log(sum2);
+for (; a > 0; a = Math.floor(a / 10)) {
+	console.log(a % 10);
+	sum2 += a % 10;
+}
+console.log(sum2);
 
 // 2
 
-// let num = 1222;
-// let sum3 = 1;
+let num = 1222;
+let sum3 = 1;
 
-// for (; num > 0; num = Math.floor(num / 10)) {
-// 	console.log(num % 10);
-// 	sum3 *= num % 10;
-// }
-// console.log(sum3);
-// let num2 = 45;
-// let sum4 = 1;
+for (; num > 0; num = Math.floor(num / 10)) {
+	console.log(num % 10);
+	sum3 *= num % 10;
+}
+console.log(sum3);
+let num2 = 45;
+let sum4 = 1;
 
-// while (num2 > 0) {
-// 	console.log(num2 % 10);
-// 	sum4 *= num2 % 10;
+while (num2 > 0) {
+	console.log(num2 % 10);
+	sum4 *= num2 % 10;
 
-// 	num2 = Math.floor(num2 / 10);
-// }
-// console.log(sum4);
+	num2 = Math.floor(num2 / 10);
+}
+console.log(sum4);
 
 // 3
 
